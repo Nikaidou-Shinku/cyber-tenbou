71aed1594e93a7c98dfc9fc5d6ef5460cac5a13a
yurzhang <yurzhang@163.com>
2023-11-15 05:21:44 +0800

    feat: pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 /* @refresh reload */
+import { registerSW } from "virtual:pwa-register";
 import { render } from "solid-js/web";
 import { Router } from "@solidjs/router";
 import App from "./App";
 import "./index.css";
+
+registerSW({ immediate: true });
 
 const root = document.getElementById("root");
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,49 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png}"],
+      },
+      includeAssets: ["favicon.png", "apple-touch-icon.png"],
+      manifest: {
+        name: "Cyber Tenbou",
+        short_name: "Cyber Tenbou",
+        description: "什么年代了还在用传统点棒？？",
+        theme_color: "#2b5696",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "~": resolve(__dirname, "src"),
